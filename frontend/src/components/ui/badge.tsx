c51cf4f05b57ae4571b2e18ec35d183994964a34
Jan-Felix Abellera <jfabellera@gmail.com>
2026-07-22 00:45:56 -0500

    Make badges dark in darkmode

--- frontend/src/components/ui/badge.tsx
+++ frontend/src/components/ui/badge.tsx
@@ -1,6 +1,6 @@
-import type * as React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { Slot } from 'radix-ui';
+import type * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
@@ -10,8 +10,7 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default: 'bg-primary text-primary-foreground [a&]:hover:bg-primary/90',
-        secondary:
-          'bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
+        secondary: 'bg-muted text-foreground [a&]:hover:bg-muted/80',
         destructive:
           'bg-destructive text-white focus-visible:ring-destructive/20 dark:bg-destructive/60 dark:focus-visible:ring-destructive/40 [a&]:hover:bg-destructive/90',
         outline:
